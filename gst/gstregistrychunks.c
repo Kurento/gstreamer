adaa8b5fff007ef6ef7e6425752eb2196e5f6ca8
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2015-10-26 11:45:54 +0100

    Revert "tracer: initial prototype for the tracing subsystem"
    This reverts commit dd69d029d1aa3c5f24e879c3d4906e9907e141c1.

--- gst/gstregistrychunks.c
+++ gst/gstregistrychunks.c
@@ -31,8 +31,6 @@
 #include <gst/gst_private.h>
 #include <gst/gstconfig.h>
 #include <gst/gstelement.h>
-#include <gst/gsttracer.h>
-#include <gst/gsttracerfactory.h>
 #include <gst/gsttypefind.h>
 #include <gst/gsttypefindfactory.h>
 #include <gst/gstdeviceproviderfactory.h>
@@ -352,13 +350,6 @@ gst_registry_chunks_save_feature (GList ** list, GstPluginFeature * feature)
     /* pack element metadata strings */
     gst_registry_chunks_save_string (list,
         gst_structure_to_string (factory->metadata));
-  } else if (GST_IS_TRACER_FACTORY (feature)) {
-    /* Initialize with zeroes because of struct padding and
-     * valgrind complaining about copying unitialized memory
-     */
-    pf = g_slice_new0 (GstRegistryChunkPluginFeature);
-    pf_size = sizeof (GstRegistryChunkPluginFeature);
-    chk = gst_registry_chunks_make_data (pf, pf_size);
   } else {
     GST_WARNING_OBJECT (feature, "unhandled feature type '%s'", type_name);
   }
@@ -686,7 +677,6 @@ gst_registry_chunks_load_feature (GstRegistry * registry, gchar ** in,
     GST_DEBUG
         ("Reading/casting for GstRegistryChunkPluginFeature at address %p",
         *in);
-
     unpack_element (*in, dmf, GstRegistryChunkDeviceProviderFactory, end, fail);
 
     pf = (GstRegistryChunkPluginFeature *) dmf;
@@ -702,12 +692,6 @@ gst_registry_chunks_load_feature (GstRegistry * registry, gchar ** in,
         goto fail;
       }
     }
-  } else if (GST_IS_TRACER_FACTORY (feature)) {
-    align (*in);
-    GST_DEBUG
-        ("Reading/casting for GstRegistryChunkPluginFeature at address %p",
-        *in);
-    unpack_element (*in, pf, GstRegistryChunkPluginFeature, end, fail);
   } else {
     GST_WARNING ("unhandled factory type : %s", G_OBJECT_TYPE_NAME (feature));
     goto fail;
